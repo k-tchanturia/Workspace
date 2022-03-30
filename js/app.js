fd60385059d1254fc762fdb34e8e6cbda1aaa5df
k.tchanturia <k.tchanturia@outlook.com>
2022-03-30 19:39:02 -0400

    Adding text to HTML

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ $('#flashMessage').hide()
     .slideDown(1000)
     .delay(3000)
     .slideUp();
+
+const title = "My First Blog Post";
+const constent = "This is my <strong>first</strong> post!";
+
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').html(constent);
