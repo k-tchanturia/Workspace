cf089b373219f6640eebeccca75315ea5861f486
k.tchanturia <k.tchanturia@outlook.com>
2022-03-31 11:38:56 -0400

    Some changes in JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-                                            //Hide the spoiler text
+//Create the "Reveal Spoiler" button
+//Append to web page
+
+//Hide the spoiler text
 $('.spoiler span').hide();
                                             //When the buttin is presed
 $('.spoiler button').click(function(){
     $('.spoiler span').show();             //Show the poler text
     $('.spoiler button').hide();           //Hide the "Reval Spiler" button
 })
+
 
