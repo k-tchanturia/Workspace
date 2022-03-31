0dbb6ce446e89cec81bfab0abbb023facf27fd41
k.tchanturia <k.tchanturia@outlook.com>
2022-03-31 11:37:41 -0400

    Update files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-
-
-
-
+                                            //Hide the spoiler text
+$('.spoiler span').hide();
+                                            //When the buttin is presed
+$('.spoiler button').click(function(){
+    $('.spoiler span').show();             //Show the poler text
+    $('.spoiler button').hide();           //Hide the "Reval Spiler" button
+})
 
